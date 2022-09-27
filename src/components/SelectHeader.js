062d8c4c0e92124a76898a13f4354323f0a05f6a
toanb1805926 <toanb1805926@student.ctu.edu.vn>
2022-09-27 22:13:04 +0700

    build base layout

---
+++ src/components/SelectHeader.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function SelectHeader(props) {
+  const [agent, setAgent] = React.useState('');
+
+  const label = props.label;
+  const itemList = props.itemList || [{title:'', value: ''}];
+
+  const handleChange = (event) => {
+    setAgent(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth size="small">
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={agent}
+          label={label}
+          onChange={handleChange}
+        >
+            {
+                itemList.map((item, indx) => (
+                    <MenuItem key={indx} value={item.value}>{item.title}</MenuItem>
+                ))
+            }
+          {/* <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem> */}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
